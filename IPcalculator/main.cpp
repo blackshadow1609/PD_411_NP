eefd6efe5c17cf4ee282b706a6702bb6a3b44563
Sky_Time87 <blackshadow1609@yandex.ru>
2025-11-20 15:54:13 +0300

    SpinPrefix DONE

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -1,7 +1,32 @@
 #include<Windows.h>
+#include<CommCtrl.h>
+#include"resource.h"
+
+#pragma comment(lib, "Comctl32.lib")
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);					
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
 	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		InitCommonControls();
+		HWND hSpinPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
+		SendMessage(hSpinPrefix, UDM_SETRANGE, 0, 32);
+	}
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
 }
