5de9d38cda2ecfb9e045b2bf43896e819b3c0e52
Sky_Time87 <blackshadow1609@yandex.ru>
2025-11-22 10:04:13 +0300

    Mask influence Prefix

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -30,26 +30,37 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		DWORD dwIPaddress = 0;
+		DWORD dwIPmask = UINT_MAX;
+		DWORD dwIPprefix = 0;
+		CHAR szIPprefix[3] = {};
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
 		{
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
-				DWORD dwIPaddress = 0;
-				DWORD dwIPmask = UINT_MAX;
-				DWORD dwIPprefix = 0;
 				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
 				DWORD dwFirst = FIRST_IPADDRESS(dwIPaddress);
 				if (dwFirst < 128) dwIPprefix = 8;
 				else if (dwFirst < 192)dwIPprefix = 16;
 				else if (dwFirst < 224)dwIPprefix = 24;
 
-				CHAR szIPprefix[3] = {};
 				sprintf(szIPprefix, "%i", dwIPprefix);
 				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
 				dwIPmask <<= (32 - dwIPprefix);
 				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+			}
+		}
+			break;
+		case IDC_IPMASK:
+		{
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+				for (; dwIPmask; dwIPmask <<= 1)dwIPprefix++;
+				sprintf(szIPprefix, "%i", dwIPprefix);
+				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
 			}
 		}
 			break;
